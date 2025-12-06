129ae5bc2fd75d6cafec6810378931e64307cd1c
Filip Wolski <filip9315@gmail.com>
2025-12-06 13:18:21 +0100

    init

---
+++ src/main/java/pl/agh/edu/to/aleksandria/AleksandriaApplication.java
@@ -0,0 +1,13 @@
+package pl.agh.edu.to.aleksandria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AleksandriaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AleksandriaApplication.class, args);
+    }
+
+}
