30a81ce67e5a1f92c24160ad6b1122b34115f706
Adrian Suliga <adriansuliga81@gmail.com>
2025-12-09 13:03:19 +0100

    Add more data to TestConfiguration

--- src/main/java/pl/agh/edu/to/aleksandria/TestConfiguration.java
+++ src/main/java/pl/agh/edu/to/aleksandria/TestConfiguration.java
@@ -43,21 +43,54 @@ public class TestConfiguration {
         Role librarianRole = roleRepository.findByName("librarian").get();
         Role adminRole = roleRepository.findByName("admin").get();
 
-        User testUser = new User("Jan", "Testowy", "ul. Tymczasowa 1/2", "jan.testowy@poczta.pl", passwordEncoder.encode("test1234"), readerRole);
-        User testEmployee = new User("Adam", "Bibliotekarz", "ul. Kawiory 21", "adam.biliot@gmail.com", passwordEncoder.encode("test1234"), librarianRole);
-        User testAdmin = new User("Marta", "Admin", "ul. Admina 7", "admin@aleksandria.pl", passwordEncoder.encode("adminpass"), adminRole);
-        userRepository.saveAll(List.of(testUser, testEmployee, testAdmin));
+        List<User> readers = List.of(
+                new User("Adrian", "Suliga", "ul. Tymczasowa 1/2", "adrian.suliga@poczta.pl", passwordEncoder.encode("suliga"), readerRole),
+                new User("Jakub", "Więcek", "ul. Kawiory 34", "jakub.więcek@poczta.pl", passwordEncoder.encode("wiecek"), readerRole),
+                new User("Filip", "Wolski", "ul. Sienkiewicza 21", "filip.wolski@poczta.pl", passwordEncoder.encode("wolski"), readerRole)
+        );
+        userRepository.saveAll(readers);
 
-        Genre dystopia = new Genre("Dystopia");
-        Genre political = new Genre("Political");
-        genreRepository.save(dystopia);
-        genreRepository.save(political);
+        List<User> librarians = List.of(
+                new User("Adam", "Nowak", "ul. Kawiory 21", "adam.nowak@alexandria.com", passwordEncoder.encode("nowak"), librarianRole),
+                new User("Jan", "Kowalski", "ul. Słowackiego 14", "jan.kowalski@alexandria.com", passwordEncoder.encode("kowalski"), librarianRole)
+        );
+        userRepository.saveAll(librarians);
 
-        Title testTitle = new Title("Rok 1984", "George Orwell", List.of(dystopia, political));
-        titleRepository.save(testTitle);
+        User admin = new User("Marta", "Admin", "ul. Admina 7", "admin@alexandria.com", passwordEncoder.encode("adminpass"), adminRole);
+        userRepository.save(admin);
 
-        Book testBook = new Book(testTitle, false);
-        bookRepository.save(testBook);
+        List<Genre> genres = List.of(
+                new Genre("Fantastyka"), new Genre("Historia"), new Genre("Horror"),
+                new Genre("Romans"), new Genre("Science-Fiction"), new Genre("Tajemnica"),
+                new Genre("Dystopia"), new Genre("Literatura Klasyczna"), new Genre("Biografia")
+        );
+        genreRepository.saveAll(genres);
+
+        List<Title> titles = List.of(
+                new Title("Rok 1984", "George Orwell", List.of(genres.get(6))),
+                new Title("Światło, którego nie widać", "Anthony Doerr", List.of(genres.get(1))),
+                new Title("Przeminęło z wiatrem", "Margaret Mitchell", List.of(genres.get(1), genres.get(3))),
+                new Title("Drużyna Pierścienia", "John Ronald Reuel Tolkien", List.of(genres.getFirst())),
+                new Title("Pan Tadeusz", "Adam Mickiewicz", List.of(genres.get(7)))
+        );
+        titleRepository.saveAll(titles);
+
+        List<Book> books = List.of(
+                new Book(titles.getFirst(), true),
+                new Book(titles.getFirst(), true),
+                new Book(titles.getFirst(), true),
+                new Book(titles.get(1), true),
+                new Book(titles.get(2), true),
+                new Book(titles.get(2), true),
+                new Book(titles.get(2), true),
+                new Book(titles.get(2), true),
+                new Book(titles.get(2), true),
+                new Book(titles.get(3), true),
+                new Book(titles.get(3), true),
+                new Book(titles.getLast(), true),
+                new Book(titles.getLast(), true)
+        );
+        bookRepository.saveAll(books);
 
         System.out.println(userRepository.findAll());
         System.out.println(bookRepository.findAll());
